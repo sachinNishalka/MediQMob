d7434e52532d143601814fcb6421cbe6d7694ee0
sachinNishalka <nishalkasachin@gmail.com>
2025-04-13 16:57:36 +0530

    compleated fetching doctors for make channel

--- components/bookAnAppointment/Card.js
+++ components/bookAnAppointment/Card.js
@@ -5,12 +5,20 @@ import { Ionicons } from "@expo/vector-icons";
 function Card({ data }) {
   const navigation = useNavigation();
   return (
-    <Pressable style={({pressed})=>[styles.card, pressed && styles.pressed]} onPress={()=>{navigation.navigate("doctorDetails", {doctorId:data.id})}}>
+    <Pressable
+      style={({ pressed }) => [styles.card, pressed && styles.pressed]}
+      onPress={() => {
+        navigation.navigate("doctorDetails", { doctorId: data.id });
+      }}
+    >
       <View style={styles.imageContainer}>
-        <Image style={styles.image} source={{uri:data.profile_image_url}}></Image>
+        <Image
+          style={styles.image}
+          source={{ uri: data.profile_image_url }}
+        ></Image>
       </View>
       <View style={styles.detailsContainer}>
-        <Text style={styles.name}>{data.fullname}</Text>
+        <Text style={styles.name}>{data.fullName}</Text>
         <Text style={styles.specialization}>{data.specialization}</Text>
       </View>
       <Ionicons name="calendar" size={32} color={Colors.primaryColor} />
@@ -21,34 +29,34 @@ function Card({ data }) {
 export default Card;
 
 const styles = StyleSheet.create({
-  card:{
-    flexDirection:"row",
-    alignItems:"center",
-    padding:10,
-    backgroundColor:'#ffffff',
-    borderRadius:5,
-    marginBottom:10
+  card: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 10,
+    backgroundColor: "#ffffff",
+    borderRadius: 5,
+    marginBottom: 10,
   },
- 
-  image:{
-    height:64,
-    width:64,
+
+  image: {
+    height: 64,
+    width: 64,
   },
-  detailsContainer:{
-    flex:1,
-    marginLeft:10
+  detailsContainer: {
+    flex: 1,
+    marginLeft: 10,
   },
-  name:{
-    fontSize:16,
-    fontWeight:'bold',
-    marginBottom:5,
-    color:Colors.primaryColor
+  name: {
+    fontSize: 16,
+    fontWeight: "bold",
+    marginBottom: 5,
+    color: Colors.primaryColor,
   },
-  specialization:{
-    fontSize:12,
-    color:Colors.secondaryGreen,
+  specialization: {
+    fontSize: 12,
+    color: Colors.secondaryGreen,
   },
-  pressed:{
-    opacity:0.7
-  }
+  pressed: {
+    opacity: 0.7,
+  },
 });
