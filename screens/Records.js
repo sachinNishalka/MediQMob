176a76e8ba8ea55d023b879c1a4cf67f29a3e437
sachinNishalka <nishalkasachin@gmail.com>
2025-04-13 08:37:47 +0530

    record screen

--- screens/Records.js
+++ screens/Records.js
@@ -1,7 +1,30 @@
-import { Text } from "react-native";
+import { View, StyleSheet, FlatList } from "react-native";
+import RecordCard from "../components/records/RecordCard";
+
+
+const records = [
+  {
+    profileImage: "https://vabvequftpzqervnzclm.supabase.co/storage/v1/object/public/doctorImages//1.png",
+    title: "Dr. John Doe",
+    date: "2024-05-01",
+    prescriptionImageUri: "https://vabvequftpzqervnzclm.supabase.co/storage/v1/object/public/prescriptionImages/1.png",
+  },
+];
+
 
 function Records() {
-  return <Text>Records</Text>;
+  return (
+    <View style={styles.container}>
+        <FlatList data={records} renderItem={({item})=><RecordCard title={item.title} date={item.date} prescriptionImageUri={item.prescriptionImageUri} profileImage={item.profileImage}/>}></FlatList>
+    </View>
+  );
 }
 
 export default Records;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+  }
+});
