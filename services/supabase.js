09375aa03a9fd76e7c6f6541bdb125fd54c8f93a
sachinNishalka <nishalkasachin@gmail.com>
2025-04-17 16:59:28 +0530

    automatic login function added

--- services/supabase.js
+++ services/supabase.js
@@ -1,8 +1,26 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
+import { AppState } from "react-native";
+
 export const supabaseUrl = "https://vabvequftpzqervnzclm.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZhYnZlcXVmdHB6cWVydm56Y2xtIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczODc0NDM5MCwiZXhwIjoyMDU0MzIwMzkwfQ.oWeyiNiK-l-YDuwOPboI2v4X5XAOnAAT5BLwgKoHTiA";
 
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
+
+AppState.addEventListener("change", (state) => {
+  if (state === "active") {
+    supabase.auth.startAutoRefresh();
+  } else {
+    supabase.auth.stopAutoRefresh();
+  }
+});
 
 export default supabase;
